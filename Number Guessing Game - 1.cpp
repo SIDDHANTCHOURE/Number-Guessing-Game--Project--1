89685808cd634adc3964febc264c93a11b135a64
SIDDHANTCHOURE <145146887+SIDDHANTCHOURE@users.noreply.github.com>
2024-04-13 09:25:58 +0530

    Add files via upload

---
+++ Number Guessing Game - 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int secretNum = 7;
+  int guess = 0; // Initialize guess to avoid unpredictable behavior
+  int guessCount = 0;
+  int guessLimit = 3;
+  bool outOfGuesses = false;
+
+  while (secretNum != guess && !outOfGuesses) {
+    if (guessCount < guessLimit) {
+      cout << "Enter guess: ";
+      cin >> guess;
+      guessCount++;
+    } else {
+      outOfGuesses = true;
+    }
+  }
+
+  if (outOfGuesses) {
+    cout << "You lose!";
+  } else {
+    cout << "You win!";
+  }
+
+  return 0;
+}
